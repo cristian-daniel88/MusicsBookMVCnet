34a95d714cffd851e27a17767e4e8e3a6076bcfb
cristian-daniel88 <cristian.daniel.herrera@hotmail.com>
2023-02-27 12:27:20 +0000

    Add project files.

---
+++ MusicsBookWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MusicsBookWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
